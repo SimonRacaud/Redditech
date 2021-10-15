fd62f16c761c3772a1c6e9f17b57066faad4ce5e
simon.racaud <simon.racaud@epitech.eu>
2021-10-15 15:03:40 +0200

    feat(activity): auth activity

--- app/src/main/java/my/epi/redditech/MainActivity.kt
+++ app/src/main/java/my/epi/redditech/MainActivity.kt
@@ -10,16 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_main)
-
-        createWebView()
-    }
-
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun createWebView() {
-        // Inject login fragment
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, LoginFragment(this))
-        transaction.addToBackStack(null)
-        transaction.commit()
     }
 }
