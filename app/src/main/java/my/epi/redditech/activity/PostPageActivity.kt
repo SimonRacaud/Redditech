30afa4e1fee32c252286b470995f6e689c6c8e35
simon.racaud <simon.racaud@epitech.eu>
2021-10-16 21:22:08 +0200

    feat(post page): activity skeleton

---
+++ app/src/main/java/my/epi/redditech/activity/PostPageActivity.kt
@@ -0,0 +1,12 @@
+package my.epi.redditech.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import my.epi.redditech.R
+
+class PostPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_page)
+    }
+}
