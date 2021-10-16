88375d3e77fd3323baffc5bd742311e1d13d8a02
simon.racaud <simon.racaud@epitech.eu>
2021-10-16 19:56:50 +0200

    feat(settings): empty settings page

--- app/src/main/java/my/epi/redditech/activity/HomeActivity.kt
+++ app/src/main/java/my/epi/redditech/activity/HomeActivity.kt
@@ -1,7 +1,9 @@
 package my.epi.redditech.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import my.epi.redditech.R
 
 class HomeActivity : AppCompatActivity() {
@@ -9,5 +11,11 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val button = findViewById<Button>(R.id.settings_button)
+        button.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
