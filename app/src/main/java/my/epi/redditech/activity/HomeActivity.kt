f27d7fe4e91f66a9f43405175dc1df50b7c98f77
simon.racaud <simon.racaud@epitech.eu>
2021-10-16 18:45:35 +0200

    feat(home): in post list tab, post list items

--- app/src/main/java/my/epi/redditech/activity/HomeActivity.kt
+++ app/src/main/java/my/epi/redditech/activity/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import my.epi.redditech.R
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
