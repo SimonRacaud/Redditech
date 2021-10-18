2a8fcba322fab10dc64083e62bb4787ed51b54fa
simon.racaud <simon.racaud@epitech.eu>
2021-10-18 14:14:18 +0200

    refactor(gui): search page list item adapter, autofocus search bar

--- app/src/main/java/my/epi/redditech/activity/SearchActivity.kt
+++ app/src/main/java/my/epi/redditech/activity/SearchActivity.kt
@@ -4,7 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.SearchView
+import androidx.recyclerview.widget.RecyclerView
 import my.epi.redditech.R
+import my.epi.redditech.adapter.SubredditListAdapter
+import my.epi.redditech.model.SubredditItemModel
+
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,21 @@ class SearchActivity : AppCompatActivity() {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
+
+        // SEARCH BAR
+        val searchBar = findViewById<SearchView>(R.id.searchInput)
+        searchBar.isIconified = false
+        searchBar.isFocusedByDefault = true
+
+        // SUBREDDIT LIST
+        // TODO : debug data (api call)
+        val subList = arrayListOf<SubredditItemModel>()
+        subList.add(SubredditItemModel("Titre du sub", "https://styles.redditmedia.com/t5_2fwo/styles/communityIcon_1bqa1ibfp8q11.png?width=256&s=45361614cdf4a306d5510b414d18c02603c7dd3c"))
+        subList.add(SubredditItemModel("Titre du sub 2", ))
+        subList.add(SubredditItemModel("Titre du sub de test 3"))
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = SubredditListAdapter(this, subList, R.layout.home_tab_subreddit_item)
+
     }
 }
