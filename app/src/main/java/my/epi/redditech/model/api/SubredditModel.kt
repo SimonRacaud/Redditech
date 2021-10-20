075c1c442ba189f6a9dbc2c54b27a75dddb75dfa
simon.racaud <simon.racaud@epitech.eu>
2021-10-20 10:58:53 +0200

    refactor(models): clean models content

--- app/src/main/java/my/epi/redditech/model/api/SubredditModel.kt
+++ app/src/main/java/my/epi/redditech/model/api/SubredditModel.kt
@@ -1,38 +1,41 @@
 package my.epi.redditech.model.api
 
 data class SubredditModel (
-    val accept_followers: Boolean,
     val banner_img: String,
     val banner_size: List<Int>?,
-    val coins: Int,
-    val default_set: Boolean,
-    val description: String,
-    val disable_contributor_requests: Boolean,
-    val display_name: String,
-    val display_name_prefixed: String,
-    val free_form_reports: Boolean,
-    val header_img: String?,
-    val header_size: List<Int>?,
-    val icon_color: String,
-    val icon_img: String,
-    val icon_size: List<Int>,
+    val banner_background_image: String?,
+    val banner_background_color: String?,
     val is_default_banner: Boolean,
-    val is_default_icon: Boolean,
-    val key_color: String,
-    val link_flair_enabled: Boolean,
-    val link_flair_position: String,
-    val name: String,
-    val over_18: Boolean,
+    val mobile_banner_image: String?,
+
+    val header_img: String?,     // header url : very small header img
+    val header_size: List<Int>?, // header
+    val header_title: String?,
+
     val primary_color: String,
-    val public_description: String,
-    val restrict_commenting: Boolean,
-    val restrict_posting: Boolean,
-    val show_media: Boolean,
-    val submit_link_label: String,
-    val submit_text_label: String,
-    val subreddit_type: String,
-    val subscribers: Int,
-    val title: String,
-    val url: String,
-    val user_is_subscriber: Boolean
+    val key_color: String,
+
+    val is_default_icon: Boolean,
+    val icon_color: String,
+    val icon_img: String,           // icon url (may be empty)
+    val icon_size: List<Int>,       // icon size
+    val community_icon: String?,    // icon url 2 (may be empty)
+
+    val accept_followers: Boolean,  // allow subscribe ?
+    val subscribers: Int,           // nb subscribers
+    val user_is_subscriber: Boolean,// subscribed
+    val url: String,    // page url
+    val name: String,   // page unique id (listing)
+    val id: String,
+
+    val public_description: String,     // description (short format)
+    val description: String,            // description (long format)
+    val description_html: String?,
+    val public_description_html: String?,
+    val display_name: String,           // title
+    val title: String,                  // full title
+    val display_name_prefixed: String,  // sub title
+
+    val created: Float?,
+    val created_utc: Float?
 )
