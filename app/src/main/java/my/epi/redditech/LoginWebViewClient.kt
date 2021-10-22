166f8839ccdafbafe83b17062ba3786e21fb594a
simon.racaud <simon.racaud@epitech.eu>
2021-10-22 12:53:06 +0200

    feat(style): clean oauth web page style sheet

--- app/src/main/java/my/epi/redditech/LoginWebViewClient.kt
+++ app/src/main/java/my/epi/redditech/LoginWebViewClient.kt
@@ -18,8 +18,7 @@ internal class LoginWebViewClient(val primaryUrl: String, val context: AuthActiv
         println(uri.host + " " + uri.path)
         println("URL = $url")
 
-        if (uri.host == "www.reddit.com" && uri.path != null
-            && (uri.path!!.startsWith("/api", false)
+        if (uri.host == "www.reddit.com" && (uri.path!!.startsWith("/api", false)
                     || uri.path!!.startsWith("/login", false))) {
             return false // This is my web site, so do not override; let my WebView load the page
         } else if (uri.host == "www.reddit.com" && uri?.path == "/") {
@@ -43,5 +42,33 @@ internal class LoginWebViewClient(val primaryUrl: String, val context: AuthActiv
             println("Log: token not found")
         }
         return true
+    }
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+        super.onPageFinished(view, url)
+        val uri = Uri.parse(url)
+
+        if (uri.host == "www.reddit.com" && uri.path!!.startsWith("/api/v1/authorize", false)) {
+            view?.loadUrl(
+                """javascript:(function f() {
+                        document.getElementById("header").style.display = "none";
+                        document.getElementsByClassName("footer-parent")[0].style.display = "none";
+                        document.getElementsByClassName("oauth2-authorize")[0].style.paddingLeft = "0px";
+                        document.getElementsByClassName("oauth2-authorize")[0].style.background = "none";
+                        document.getElementsByClassName("icon")[0].style.display = "none";
+                        var divNode = document.createElement("div");
+                        divNode.innerHTML = "<br><style>
+                            .access {
+                              width: 90vw !important;
+                              float: none !important;;
+                            }
+                            .oauth2-authorize h1 {
+                              width: 90vw;
+                            }
+                            </style>";
+                        document.body.appendChild(divNode);
+                    })()"""
+            )
+        }
     }
 }
